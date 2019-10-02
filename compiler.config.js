02a63687e0b4f3bb05d2cac597ce98cdf4836d0d
Kyle Andrews <kylea@page.works>
2019-10-02 14:35:55 -0400

    Hooks up sass compiler

--- compiler.config.js
+++ compiler.config.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const glob = require('glob');
+const sass = require('node-sass');
 
 class CustomCompiler
 {
@@ -12,20 +13,101 @@ class CustomCompiler
     {
         try
         {
+            /** HTML */
             const htmlFiles = await this.getHtmlFiles();
             if (htmlFiles.length === 0)
             {
                 throw "Couldn't find any HTML files in the src/ directory";
             }
-
             await this.buildHtmlDirectories(htmlFiles);
             await this.cloneHtmlFiles(htmlFiles);
+
+            /** SASS */
+            const sassFiles = await this.getSassFiles();
+            if (sassFiles.length === 0)
+            {
+                throw "Couldn't find any SCSS files in the src/ directory";
+            }
+            await this.buildScssDirectory();
+            await this.renderCss(sassFiles);
+
             console.log('Success!');
         }
         catch (error)
         {
             console.log(error);
         }
+    }
+
+    renderCss(files)
+    {
+        return new Promise((resolve, reject) => {
+            let rendered = 0;
+            for (let i = 0; i < files.length; i++)
+            {
+                const sourcePath = files[i];
+                const destPath = sourcePath.replace('src', 'build/stylesheets').replace(/(scss)$/, 'css').trim().toLowerCase();
+
+                sass.render(
+                    {
+                        file: sourcePath,
+                        outputStyle: 'compressed'
+                    },
+                    (error, result) => {
+                        if (error)
+                        {
+                            reject(`\n\n${ error.message } at line ${ error.line } ${ error.file }\n\n`);
+                        }
+
+                        if (result === null)
+                        {
+                            reject(`\n\n${ sourcePath } failed to render\n\n`);
+                        }
+
+                        fs.writeFile(destPath, result.css.toString(), (error) => {
+                            if (error)
+                            {
+                                reject(error);
+                            }
+
+                            rendered++;
+                            if (rendered === files.length)
+                            {
+                                resolve();
+                            }
+                        });
+                    }
+                )
+            }
+        });
+    }
+
+    buildScssDirectory()
+    {
+        return new Promise((resolve, reject) => {
+            fs.mkdir('build/stylesheets', (error) => {
+                if (error)
+                {
+                    reject(error);
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    getSassFiles()
+    {
+        return new Promise((resolve, reject) => {
+            glob('src/**/*.scss', (error, files) => {
+                if (error)
+                {
+                    reject(error);
+                }
+
+                resolve(files);
+            });
+        });
     }
 
     buildHtmlDirectories(files)
